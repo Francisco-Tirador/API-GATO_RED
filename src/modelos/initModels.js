68eb7ca3ff0aa8490b60c9baf54dd9f6915cd7bc
Francisco Beltran <roshlandia@gmail.com>
2022-10-07 19:45:40 -0500

    model Validation

--- src/modelos/initModels.js
+++ src/modelos/initModels.js
@@ -4,11 +4,16 @@ const poust=require('./poust.model')
 const redCombersation=require('./redCombersation.model')
 const Roles=require('./rol.model')
 const Users=require('./user.model')
-
+const Validation=require('./validation.model')
 //TODO // hasMANY el primero tiene la pk
 //TODO // belog el primero tiene la fk
 
 const initModels=()=>{
+
+    combersations.hasMany(Validation)       
+    Validation.belongsTo(combersations)
+    
+
 Roles.hasMany(Users,{foreignKey:'roleId'})       
 Users.belongsTo(Roles)
 
